9f8520f6fa6fc4a76cd1467c7b9bba309cf92038
artslob <artslob@yandex.ru>
2022-02-23 18:57:44 +0300

    add func to generate next generation of cells

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
+use itertools::Itertools;
 use macroquad::prelude::*;
 
 const CELL_COUNT: i32 = 5;
 
 #[macroquad::main("BasicShapes")]
 async fn main() {
+    let life_dead = [CellState::Life, CellState::Dead];
+    let cell_row = (0..CELL_COUNT)
+        .zip(life_dead.iter().cycle())
+        .map(|(_, state)| Cell { state: *state })
+        .collect::<Vec<_>>();
+
+    let mut cells = (0..CELL_COUNT)
+        .map(|_| cell_row.clone())
+        .collect::<Vec<_>>();
+
     loop {
         clear_background(BLACK);
 
@@ -17,24 +28,9 @@ async fn main() {
 
         let cell_width = square_width / CELL_COUNT as f32;
 
-        let true_false = [CellState::Life, CellState::Dead];
-        let cell_row = (0..CELL_COUNT)
-            .zip(true_false.iter().cycle())
-            .map(|(_, state)| Cell { state: *state })
-            .collect::<Vec<_>>();
-
-        let cells = (0..CELL_COUNT)
-            .map(|_| cell_row.clone())
-            .collect::<Vec<_>>();
-
         for (i, row) in cells.iter().enumerate() {
             for (j, cell) in row.iter().enumerate() {
-                let skip = if i % 2 == 0 {
-                    !matches!(cell.state, CellState::Life)
-                } else {
-                    matches!(cell.state, CellState::Life)
-                };
-                if skip {
+                if matches!(cell.state, CellState::Dead) {
                     continue;
                 }
                 let cell_x = x + (j as f32 * cell_width);
@@ -44,8 +40,42 @@ async fn main() {
             }
         }
 
+        cells = calculate_next_generation(&cells);
+
         next_frame().await
     }
+}
+
+fn calculate_next_generation(current: &[Vec<Cell>]) -> Vec<Vec<Cell>> {
+    let mut result = vec![];
+    for (i, row) in current.iter().enumerate() {
+        let mut next_row = vec![];
+        for (j, cell) in row.iter().enumerate() {
+            match cell.state {
+                CellState::Dead => {
+                    let left_bound = i.checked_sub(1).unwrap_or_default();
+                    let right_bound = (i + 1).min(current.len() - 1);
+                    let upper_bound = j.checked_sub(1).unwrap_or_default();
+                    let lower_bound = (j + 1).min(row.len() - 1);
+                    let alive_cells_count = (left_bound..right_bound)
+                        .cartesian_product(upper_bound..lower_bound)
+                        .filter(|(left, right)| left == right)
+                        .map(|(a, b)| &current[a][b])
+                        .filter(|cell| matches!(cell.state, CellState::Life))
+                        .count();
+                    let state = if alive_cells_count == 3 {
+                        CellState::Life
+                    } else {
+                        CellState::Dead
+                    };
+                    next_row.push(Cell {state});
+                }
+                CellState::Life => {}
+            }
+        }
+        result.push(next_row);
+    }
+    result
 }
 
 #[derive(Debug, Clone)]
