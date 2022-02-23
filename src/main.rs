0beefd5e92fa2d15b6e48e889b7871df3383e173
artslob <artslob@yandex.ru>
2022-02-23 19:13:17 +0300

    update generation once a second

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,27 @@ const CELL_COUNT: i32 = 5;
 
 #[macroquad::main("BasicShapes")]
 async fn main() {
-    let life_dead = [CellState::Life, CellState::Dead];
-    let cell_row = (0..CELL_COUNT)
-        .zip(life_dead.iter().cycle())
-        .map(|(_, state)| Cell { state: *state })
-        .collect::<Vec<_>>();
+    // let life_dead = [CellState::Life, CellState::Dead];
+    // let cell_row = (0..CELL_COUNT)
+    //     .zip(life_dead.iter().cycle())
+    //     .map(|(_, state)| Cell { state: *state })
+    //     .collect::<Vec<_>>();
+    //
+    // let mut cells = (0..CELL_COUNT)
+    //     .map(|_| cell_row.clone())
+    //     .collect::<Vec<_>>();
 
-    let mut cells = (0..CELL_COUNT)
-        .map(|_| cell_row.clone())
-        .collect::<Vec<_>>();
+    let mut cells: Vec<Vec<Cell>> = (0..CELL_COUNT)
+        .map(|_| (0..CELL_COUNT).map(|_| Cell {state: CellState::Dead}).collect())
+        .collect();
+
+    cells[0][1] = Cell {state: CellState::Life};
+    cells[1][2] = Cell {state: CellState::Life};
+    cells[2][0] = Cell {state: CellState::Life};
+    cells[2][1] = Cell {state: CellState::Life};
+    cells[2][2] = Cell {state: CellState::Life};
+
+    let mut time = get_time();
 
     loop {
         clear_background(BLACK);
@@ -40,7 +52,10 @@ async fn main() {
             }
         }
 
-        cells = calculate_next_generation(&cells);
+        if get_time() - time > 1. {
+            cells = calculate_next_generation(&cells);
+            time = get_time();
+        }
 
         next_frame().await
     }
@@ -59,7 +74,7 @@ fn calculate_next_generation(current: &[Vec<Cell>]) -> Vec<Vec<Cell>> {
                     let lower_bound = (j + 1).min(row.len() - 1);
                     let alive_cells_count = (left_bound..right_bound)
                         .cartesian_product(upper_bound..lower_bound)
-                        .filter(|(left, right)| left == right)
+                        .filter(|(left, right)| left != right)
                         .map(|(a, b)| &current[a][b])
                         .filter(|cell| matches!(cell.state, CellState::Life))
                         .count();
@@ -68,9 +83,25 @@ fn calculate_next_generation(current: &[Vec<Cell>]) -> Vec<Vec<Cell>> {
                     } else {
                         CellState::Dead
                     };
-                    next_row.push(Cell {state});
+                    next_row.push(Cell { state });
                 }
-                CellState::Life => {}
+                CellState::Life => {
+                    let left_bound = i.checked_sub(1).unwrap_or_default();
+                    let right_bound = (i + 1).min(current.len() - 1);
+                    let upper_bound = j.checked_sub(1).unwrap_or_default();
+                    let lower_bound = (j + 1).min(row.len() - 1);
+                    let alive_cells_count = (left_bound..right_bound)
+                        .cartesian_product(upper_bound..lower_bound)
+                        .filter(|(left, right)| left != right)
+                        .map(|(a, b)| &current[a][b])
+                        .filter(|cell| matches!(cell.state, CellState::Life))
+                        .count();
+                    let state = match alive_cells_count {
+                        2 | 3 => CellState::Life,
+                        _ => CellState::Dead,
+                    };
+                    next_row.push(Cell { state });
+                }
             }
         }
         result.push(next_row);
